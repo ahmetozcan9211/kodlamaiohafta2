aadf7f74c2dea2ceffad8a6b4fc600f6da30c0fa
ahmetozcan9211 <4ahmetozcan@gmail.com>
2022-09-24 14:48:18 +0300

    ders 25 son

---
+++ src/methods2/Main.java
@@ -0,0 +1,18 @@
+package methods2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void ekle(){
+        System.out.println("eklendi");
+    }
+
+    public static void sil(){
+        System.out.println("Silindi");
+    }
+    public static void guncelle(){
+        System.out.println("güncellendi ");
+    }
+}
