cdb908a63a2faf264564af9d8b380b579a48e0fd
ahmetozcan9211 <4ahmetozcan@gmail.com>
2022-09-24 15:54:23 +0300

    ders 29 son

--- src/methods2/Main.java
+++ src/methods2/Main.java
@@ -2,6 +2,8 @@ package methods2;
 
 public class Main {
     public static void main(String[] args) {
+        int toplam=topla(1,2,3,4,5,6,7,8);
+        System.out.println(toplam);
 
     }
 
@@ -14,5 +16,14 @@ public class Main {
     }
     public static void guncelle(){
         System.out.println("güncellendi ");
+    }
+    public static int topla(int... sayilar){
+        int toplam=0;
+
+        for (int sayi:sayilar){
+            toplam+=sayi;
+        }
+        return toplam;
+
     }
 }
